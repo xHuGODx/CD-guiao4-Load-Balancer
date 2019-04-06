8f7edf4c5729e0b69433e3fc59f6fbdcff888a92
Diogo Gomes <dgomes@av.it.pt>
2019-04-06 22:51:26 +0100

    works

--- load_balancer.py
+++ load_balancer.py
@@ -2,37 +2,80 @@
 
 import socket
 import logging
-
+import select
 
 # configure logger output format
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
 
+sockets = []
+socket_pairs = {}
+
+
+def remote_conn():
+    try:
+        remote_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        remote_sock.connect(('127.0.0.1', 5000))
+        return remote_sock
+    except Exception as e:
+        return None 
 
 def main(addr, servers):
     # create a logger
     logger = logging.getLogger('Load Balancer')
-    # connect to the back end servers
-    client_sockets = []
-    for server in servers:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(server)
-        client_sockets.append(s)
-    # open a socket for the clients
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    logger.info('Bind address %s', addr)
-    server_socket.bind(addr)
-    server_socket.listen(1)
-    while True:
-        conn, addr = server_socket.accept()
-        logger.info('New Connection %s', addr)
-        request = conn.recv(4096)
-        logger.debug("%s", request)
-        client_sockets[0].send(request)
-        reply = client_sockets[0].recv(4096)
-        logger.debug("%s", reply)
-        conn.send(reply)
-        conn.close()
-    return 0
+    global sockets
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    try:
+        sock.setblocking(False)
+        sock.bind(addr)
+        sock.listen(3)
+        sockets.append(sock)
+        logger.debug('Listening on {0} {1}'.format(*addr))
+        while True:
+            readable, writable, exceptional = select.select(sockets, [], [])
+            for s in readable:
+                if s == sock:
+                    client, addr = sock.accept()
+                    logger.debug('Accepted connection {0} {1}'.format(*addr))
+                    client.setblocking(False)
+                    sockets.append(client)
+                    socket_pairs[client] = remote_conn()
+                    sockets.append(socket_pairs[client])
+                if s in socket_pairs:
+                    logger.debug('Client data')
+                    while True:
+                        data = s.recv(4096)
+                        if len(data) == 0:
+                            sockets.remove(socket_pairs[s])
+                            socket_pairs[s].close()
+                            sockets.remove(s)
+                            s.close()
+                        if not data:
+                            break
+                        print(data)
+                        if s in socket_pairs:
+                            socket_pairs[s].send(data)
+                            break
+                else:
+                    for c, upstream in socket_pairs.items():
+                        if s == upstream:
+                            logger.debug("Upstream data")
+                            while True:
+                                try:
+                                    data = s.recv(4096)
+                                    print(len(data))
+                                    if not data:
+                                        break
+                                    print(data)
+                                    c.send(data)
+                                    break
+                                except Exception:
+                                    # Socket already closed
+                                    break
+    except Exception as err:
+        logger.error(err)
 
 
 if __name__ == '__main__':
