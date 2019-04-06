0f9cb8750d6755525c28da09ca36015edbfef00e
Diogo Gomes <dgomes@av.it.pt>
2019-04-06 23:58:49 +0100

    refactor 2

--- load_balancer.py
+++ load_balancer.py
@@ -6,6 +6,7 @@ import select
 
 # configure logger output format
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
+logger = logging.getLogger('Load Balancer')
 
 class SocketMapper:
     def __init__(self):
@@ -14,14 +15,13 @@ class SocketMapper:
     def add(self, client_sock, upstream_server=('127.0.0.1', 5000)):
         upstream_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         upstream_sock.connect(upstream_server)
+        logger.debug("Proxying to %s %s", *upstream_server)
         self.map[client_sock] =  upstream_sock
 
     def delete(self, sock):
         try:
-            if sock in self.map:
-                self.map[sock].close()
-                del self.map[sock]
-                sock.close() 
+            self.map.pop(sock)
+            sock.close() 
         except KeyError:
             pass
 
@@ -34,12 +34,11 @@ class SocketMapper:
         return None
 
     def get_all_socks(self):
-        return list(self.map.keys()) + list(self.map.values())
+        """ Flatten all sockets into a list"""
+        return list(sum(self.map.items(), ())) 
+
 
 def main(addr, servers):
-    # create a logger
-    logger = logging.getLogger('Load Balancer')
-
     mapper = SocketMapper()
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,25 +48,23 @@ def main(addr, servers):
         sock.setblocking(False)
         sock.bind(addr)
         sock.listen(3)
-        logger.debug('Listening on {0} {1}'.format(*addr))
+        logger.debug("Listening on %s %s", *addr)
         while True:
             readable, writable, exceptional = select.select([sock]+mapper.get_all_socks(), [], [])
             for s in readable:
                 if s == sock:
                     client, addr = sock.accept()
-                    logger.debug('Accepted connection {0} {1}'.format(*addr))
+                    logger.debug("Accepted connection %s %s", *addr)
                     client.setblocking(False)
                     mapper.add(client) #Falta upstream server
                 if mapper.get_sock(s):
                     data = s.recv(4096)
-                    if len(data) == 0:
-                        logger.debug("closing session %s", s)
+                    if len(data) == 0: # No messages in socket, we can close down the socket
                         mapper.delete(s)
                         continue
                     mapper.get_sock(s).send(data)
     except Exception as err:
         logger.error(err)
-
 
 if __name__ == '__main__':
     main(('127.0.0.1', 8080),[('localhost', 5000)])
